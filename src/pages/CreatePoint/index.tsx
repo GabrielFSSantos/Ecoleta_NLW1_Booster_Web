c98159cb8f4acdb7c9c71470cd0bdb85b3bb0a8a
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-11-10 11:47:32 -0300

    Create Routes and Link

---
+++ src/pages/CreatePoint/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CreatePoint = () => {
+  
+  return(
+    <h1>The Father is ON!!! </h1>
+  );
+}
+
+export default CreatePoint;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {FiLogIn} from 'react-icons/fi';
-import './styles.css';
+import {Link} from 'react-router-dom';
 
+import './styles.css';
 import logo from '../../assets/logo.svg';
 
 const Home = () => {
@@ -17,10 +18,10 @@ const Home = () => {
           <h1>Seu marketplace de coleta de residuos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-          <a href="/cadastro">
+          <Link to="/create-point">
             <span><FiLogIn/></span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
