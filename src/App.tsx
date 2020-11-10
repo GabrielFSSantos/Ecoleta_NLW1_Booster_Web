a7d561ea96d935c0808da455d0e95ceb19eb27e0
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-11-10 11:25:32 -0300

    Create Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter +1);
-  }
-
   return (
     <>
-      <Header title="Headerzão" />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+      <Home />
     </>
   );
 }
