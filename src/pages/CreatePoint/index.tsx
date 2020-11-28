6a6f27ae5db61628c2b46e64debff134082bac66
Gabriel Felipe Souza Santos <gabrielfelipesouzasantos@gmail.com>
2020-11-27 22:10:31 -0300

    Finished according to classes

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
-import { Link, useHistory} from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { useMapEvent, MapContainer, TileLayer, Marker } from 'react-leaflet';
-import {LeafletMouseEvent} from 'leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
 
 import api from '../../services/api';
 import './styles.css';
+import Dropzone from '../../components/Dropzone';
 import logo from '../../assets/logo.svg';
 
 interface Item {
@@ -35,17 +36,18 @@ const CreatePoint = () => {
     email: '',
     whatsapp: '',
   });
-  
+
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
-      const {latitude, longitude} = position.coords;
+      const { latitude, longitude } = position.coords;
       setInitialPosition([latitude, longitude]);
     });
   }, []);
@@ -72,16 +74,16 @@ const CreatePoint = () => {
     });
   }, [selectedUf]);
 
-  
+
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-    const {name, value} = event.target;
-    setFormData({...formData, [name]: value});
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
   }
 
   function MapEventClick() {
     useMapEvent('click', (event: LeafletMouseEvent) => {
-        setSelectedPosition([event.latlng.lat, event.latlng.lng]);
-      },
+      setSelectedPosition([event.latlng.lat, event.latlng.lng]);
+    },
     );
     return null;
   }
@@ -99,34 +101,37 @@ const CreatePoint = () => {
   function handleSelectedItem(id: number) {
     const alreadySelected = selectedItems.findIndex(item => item === id);
 
-    if(alreadySelected >= 0) {
+    if (alreadySelected >= 0) {
       const filteredItems = selectedItems.filter(item => item !== id);
 
       setSelectedItems(filteredItems);
-      
-    }else {
+
+    } else {
       setSelectedItems([...selectedItems, id]);
     }
   }
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
-    
-    const {name, email, whatsapp} = formData;
+
+    const { name, email, whatsapp } = formData;
     const uf = selectedUf;
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+    if(selectedFile) {
+      data.append('image', selectedFile);
     }
 
     await api.post('points', data);
@@ -135,7 +140,7 @@ const CreatePoint = () => {
 
     history.push('/');
   }
- 
+
   return (
     <div id="page-create-point">
       <header>
@@ -147,6 +152,8 @@ const CreatePoint = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
+
+        <Dropzone onFileUploaded={selectedFile} />
 
         <fieldset>
           <legend>
@@ -180,7 +187,7 @@ const CreatePoint = () => {
           <MapContainer center={initialPosition} zoom={15} >
             <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-            <MapEventClick/>
+            <MapEventClick />
             <Marker position={selectedPosition} />
           </MapContainer>
 
@@ -189,7 +196,7 @@ const CreatePoint = () => {
               <label htmlFor="uf">Estado (UF)</label>
               <select name="uf" id="uf" value={selectedUf} onChange={handleSelectedUF}>
                 <option value="0"> Selecione uma UF</option>
-                {ufs.map(uf => ( <option key={uf} value={uf}>{uf}</option> ))}
+                {ufs.map(uf => (<option key={uf} value={uf}>{uf}</option>))}
               </select>
             </div>
 
@@ -197,7 +204,7 @@ const CreatePoint = () => {
               <label htmlFor="city">Cidade</label>
               <select name="city" id="city" value={selectedCity} onChange={handleSelectedCity}>
                 <option value="0"> Selecione uma cidade</option>
-                {cities.map(city => ( <option key={city} value={city}>{city}</option> ))}
+                {cities.map(city => (<option key={city} value={city}>{city}</option>))}
               </select>
             </div>
           </div>
@@ -212,7 +219,7 @@ const CreatePoint = () => {
           <ul className="items-grid">
             {items.map(item => (
               <li key={item.id} onClick={() => handleSelectedItem(item.id)} className={selectedItems.includes(item.id) ? 'selected' : ''} >
-                <img src={item.image_url} alt={item.title} /> 
+                <img src={item.image_url} alt={item.title} />
                 <span>{item.title}</span>
               </li>
             ))}
